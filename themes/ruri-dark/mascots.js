4d7327c3be8a3db23a6af8c253e8bf9d16268938
cyranodebelgerac <35063310+cyranodebelgerac@users.noreply.github.com>
2018-02-19 12:43:07 +0000

    Update mascots.js

--- themes/ruri-dark/mascots.js
+++ themes/ruri-dark/mascots.js
@@ -1,1 +1,4 @@
-var mascotList = [ "ruri1.png", "ruri2.png", "ruri3.png" ];
+var mascotList = [ "ruri1.png" ];
+// if you want to have the mascot picture randomly rotate then make it something like 
+// mascotList = ["ruri1.ping", "ruri2.png", "ruri3.png" ]; 
+// naturally the filenames can be anything other than rurix.png if you want, and you can have as many as you wish too. 
